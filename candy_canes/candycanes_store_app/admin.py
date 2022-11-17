8c11b6a98eb9db1deacea560efe9245e61b9c372
Dani Cantu <daniellecantu@dovahkiin.myfiosgateway.com>
2022-11-17 00:36:39 -0500

    update

--- candy_canes/candycanes_store_app/admin.py
+++ candy_canes/candycanes_store_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product, Cart
+
+admin.site.register(Product)
+admin.site.register(Cart)
 
 # Register your models here.
--- candy_canes/candycanes_store_app/views.py
+++ candy_canes/candycanes_store_app/views.py
@@ -2,13 +2,25 @@
 #backend uses models
 #views page renders HTTP response
 from django.shortcuts import render, redirect
-from . import views
-from django.http import HttpResponse
+from .models import Product
+#from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to our store")
+    return render(request, 'pages/home.html')
 
 def checkout(request):
-    return render(request, 'candycanes_store_app/templates/pages/checkout.html')
+    print("Checkout")
+    return render(request, 'pages/checkout.html')
+
+def list_home(request):
+    home_list = Product.objects.all()  # gets all of the to do lists from the database and store them in a variable
+  
+    # creates the context dictionary to send the blog posts to the template
+    context = {
+       'home_list': home_list
+    }
+    return render(request, 'pages/home.html', context)
+
+    
 
 # Create your views here.
