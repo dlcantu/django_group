2a96a18d97c78603ffaa1e321f30b73ab3e16d83
Dani Cantu <daniellecantu@dovahkiin.myfiosgateway.com>
2022-11-16 22:24:21 -0500

    update

--- candy_canes/candycanes_store_app/models.py
+++ candy_canes/candycanes_store_app/models.py
@@ -1,14 +1,14 @@
+#models will always be classes
+#one return statement per function
 from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
-    price = models.CharField(max_length=5)
+    price = models.CharField(max_length=10)
 
-    def __str__(self):
-        return self.name
-        return self.description
-        return self.price
+    def __str__(self): #important for admin (backend)
+        return f'{self.name}, {self.description}, {self.price}'
 
 class Cart(models.Model):
     total = models.ForeignKey(Product, on_delete=models.CASCADE) #need to figure out the logic
--- candy_canes/candycanes_store_app/views.py
+++ candy_canes/candycanes_store_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+#logic and render pages
+#backend uses models
+#views page renders HTTP response
+from django.shortcuts import render, redirect
+from . import views
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to our store")
+
+def checkout(request):
+    return render(request, 'candycanes_store_app/templates/pages/checkout.html')
 
 # Create your views here.
